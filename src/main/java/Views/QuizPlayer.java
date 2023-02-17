2c54be760d822cb1b5aa26b21ea7c02741b18e72
ojastaneja7 <ojastaneja7@gmail.com>
2023-02-16 22:10:14 -0500

    Created Quiz Player Class

---
+++ src/main/java/Views/QuizPlayer.java
@@ -0,0 +1,31 @@
+package Views;
+
+import javax.swing.*;
+import Models.Flashcard;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class QuizPlayer {
+    private JTextArea display;
+
+    private JTextArea answer;
+
+    private ArrayList<Flashcard> cardList;
+
+    private JFrame frame;
+
+    private boolean isShowAnswer;
+
+    private JButton showAnswer;
+
+
+
+
+
+
+}
