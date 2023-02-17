f52e6e667ef9e37b8d6143567399851dcfb9fec1
sourav <sourav.chandhok@outlook.com>
2023-02-16 19:28:53 -0500

    EmailSender Class Added(Not working coz of dependencies issues)

---
+++ src/main/java/Models/EmailSender.java
@@ -0,0 +1,65 @@
+package Models;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class EmailSender {
+
+    public static void sendEmail(String to, String subject, String body) {
+        String from = "flashstudyhelp@gmail.com";
+        String password = "fwffotmkdmlssfmj";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+
+        //Mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getDefaultInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                        return new javax.mail.PasswordAuthentication(from, password);
+                    }
+                });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+            System.out.println("Email sent successfully.");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public static void scheduleEmailReminder(Date reminderDate, String to, String subject, String body) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                EmailSender.sendEmail(to, subject, body);
+            }
+        }, reminderDate);
+    }
+
+    public static void main(String[] args) {
+        sendEmail("sourav.chandok@gmail.com","testing flashcard","This is a test");
+    }
+
+}
