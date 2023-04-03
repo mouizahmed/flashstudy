ebe225112ef5768c73a6bad3e1f4e0d3792961eb
ojastaneja7 <ojastaneja7@gmail.com>
2023-04-03 09:40:28 -0400

    adding time to Controller

--- src/main/java/Models/QuizSession.java
+++ src/main/java/Models/QuizSession.java
@@ -13,6 +13,8 @@ public class QuizSession {
 	private QuizCreator quizCreator;
 	long durationInMins;
 
+	long durationInSeconds;
+
 	private User user;
 	private int score = 0;
 	private double avgScore;
@@ -75,6 +77,9 @@ public class QuizSession {
 	public long getdurationInMins(){
 		return this.durationInMins;
 	}
+	public long getDurationInSeconds(){
+		return this.durationInSeconds;
+	}
 	public Date getDateCreated(){
 		return this.dateCreated;
 	}
@@ -82,8 +87,7 @@ public class QuizSession {
 	public void submitQuiz() {
 		Date quizEndTime = new Date();
 		long timeTaken = quizEndTime.getTime() - quizStartTime.getTime();
-		long durationInSeconds = (timeTaken / 1000);
-		 this.durationInMins = (durationInSeconds / 60);
+		this.durationInSeconds = (timeTaken / 1000);
 
 
 		try {
--- src/main/java/Views/QuizResults.java
+++ src/main/java/Views/QuizResults.java
@@ -76,7 +76,7 @@ public class QuizResults extends JPanel {
 		avgScore.setBounds(103, 221, 257, 16);
 		add(avgScore);
 
-		JLabel timer = new JLabel("Time taken to complete the quiz:" + Integer.toString((int)quizSession.getdurationInMins()));
+		JLabel timer = new JLabel("Time taken :" + Integer.toString((int)quizSession.getDurationInSeconds()));
 		timer.setBounds(103,274,150,16);
 		add(timer);
 		
