bf9d22b604003304cb6aeebf5407c6747df1545d
ojastaneja7 <ojastaneja7@gmail.com>
2023-04-03 01:04:40 -0400

    Edited QuizSession to calculate the time taken by the user to finish the quiz.

--- src/main/java/Models/QuizSession.java
+++ src/main/java/Models/QuizSession.java
@@ -1,12 +1,16 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class QuizSession {
-	
+
 	private QuizCreator quizCreator;
-	
+
 	private User user;
 	private int score = 0;
 	private double avgScore;
@@ -14,19 +18,20 @@ public class QuizSession {
 	private String quizID;
 	private Deck deck;
 	private Date dateCreated;
-	
+	private Date quizStartTime;
+	private JDBC mysql_database;
+
 	public QuizSession(QuizCreator quizCreator, User user) {
 		this.quizCreator = quizCreator;
 		this.user = user;
 		this.questions = quizCreator.getAllQuestions();
 		this.quizID = quizCreator.getQuizID();
 		this.deck = quizCreator.getDeck();
+		this.dateCreated = new Date();
+		this.quizStartTime = new Date();
 	}
-	
+
 	public void answerQuestion(int index, String answer) {
-//		if (this.questions.get(index).checkAnswer(answer)) {
-//			this.score += 1;
-//		}
 		this.questions.get(index).checkAnswer(answer);
 	}
 
@@ -37,39 +42,62 @@ public class QuizSession {
 				this.score += 1;
 			}
 		}
-		
+
 		return this.score;
 	}
-	
-	
+
+
 	public double getAvgScore() {
 		return this.avgScore = (double) this.getScore() / (double) this.getNumQuestions();
 	}
-	
+
 	public ArrayList<Question> getAllQuestions() {
 		return this.questions;
 	}
-	
+
 	public int getNumQuestions() {
 		return quizCreator.getAllQuestions().size();
 	}
-	
+
 	public String getQuizID() {
 		return this.quizID;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
-	
+
 	public Deck getDeck() {
 		return this.deck;
 	}
-	
-//	public void submitQuiz() {
-//		this.dateCreated = new java.util.Date();
-//		// link to sql
-//	}
-	
 
+	public void submitQuiz() {
+		Date quizEndTime = new Date();
+		long timeTaken = quizEndTime.getTime() - quizStartTime.getTime();
+		int durationInSeconds = (int) (timeTaken / 1000);
+
+		try {
+
+			String url = "jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_957a5ec054245a7?reconnect=true";
+			String username = "b94e112144658f";
+			String password = "7af4eb5c";
+			Connection conn = DriverManager.getConnection(url, username, password);
+
+			// Create a prepared statement to insert the quiz result into the quiz_results table
+			String sql = "INSERT INTO quiz_results (username, quiz_id, score, time_taken, date_created) VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, user.getUsername());
+			stmt.setString(2, quizID);
+			stmt.setInt(3, score);
+			stmt.setLong(4, timeTaken);
+			stmt.setTimestamp(5, new java.sql.Timestamp(dateCreated.getTime()));
+
+			stmt.executeUpdate();
+
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
