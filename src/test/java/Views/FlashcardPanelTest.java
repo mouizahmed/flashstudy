e9091dea1f510508aed2d21732f3cdf76d7708b8
Daoud Ali <65147757+DaoudAli@users.noreply.github.com>
2023-04-07 21:26:38 -0400

    Update Image Tests

--- src/test/java/Views/FlashcardPanelTest.java
+++ src/test/java/Views/FlashcardPanelTest.java
@@ -4,32 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import Views.FlashcardPanel;
+import Models.Flashcard;
 
 class FlashcardPanelTest {
-	
 
-	@Test
-	void imageOver100kB() {
-        FlashCard flashCard = new FlashCard();
+    @Test
+    void imageOver100kB() {
+
         byte[] imgData = new byte[102400]; // creating a 100KB file
-        flashCard.setFlashCardImgData(imgData);
-        assertFalse(flashCard.isFlashCardImgValid());
+        FlashcardPanel FCP = new FlashcardPanel(null, null, null, null);
+        assertFalse(FCP.isImageFileSizeValid(imgData.length));
     }
 
     @Test
-	void emptyImage() {
-        FlashCard flashCard = new FlashCard();
-        byte[] imgData = new byte[0];
-        flashCard.setFlashCardImgData(imgData);
-        assertFalse(flashCard.isFlashCardImgValid());
-	}
+    void emptyImage() {
+        byte[] imgData = new byte[0]; // creating an empty file
+        FlashcardPanel FCP = new FlashcardPanel(null, null, null, null);
+        assertFalse(FCP.isImageFileSizeValid(imgData.length));
+    }
 
     @Test
-	void validImage() {
-        FlashCard flashCard = new FlashCard();
+    void validImage() {
         byte[] imgData = new byte[1024]; // creating a 1KB file
-        flashCard.setFlashCardImgData(imgData);
-        assertTrue(flashCard.isFlashCardImgValid());
-	}
+        FlashcardPanel FCP = new FlashcardPanel(null, null, null, null);
+        assertTrue(FCP.isImageFileSizeValid(imgData.length));
+    }
 
+    @Test
+    void invalidImageLength() {
+        FlashcardPanel FCP = new FlashcardPanel(null, null, null, null);
+        assertFalse(FCP.isImageFileSizeValid(-1));
+    }
 }
